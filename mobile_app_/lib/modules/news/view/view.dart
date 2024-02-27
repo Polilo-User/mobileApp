1e2063fd54802c90915fb936ce2101c659f6838e
Кожевников Иван <64777603+Viktor-8bit@users.noreply.github.com>
2024-02-28 01:50:44 +0700

    последний рефакторинг

--- mobile_app_/lib/modules/news/view/view.dart
+++ mobile_app_/lib/modules/news/view/view.dart
@@ -2,20 +2,4 @@
 
 
 
-import 'package:flutter/material.dart';
-
-class News extends StatefulWidget {
-  const News({ Key? key }) : super(key: key);
-
-  @override
-  _NewsState createState() => _NewsState();
-}
-
-class _NewsState extends State<News> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: const Text("Новости"),
-    );
-  }
-}
+export 'news.dart';
---
+++ mobile_app_/lib/modules/residence_complex_list/view/residence_complex_list.dart
@@ -0,0 +1,65 @@
+
+
+
+import 'package:mobile_app_/modules/residence_complex_list/widgets/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/material.dart';
+
+class ResidenceComplexList extends StatefulWidget {
+  const ResidenceComplexList({ Key? key }) : super(key: key);
+
+  @override
+  _ResidenceComplexListState createState() => _ResidenceComplexListState();
+}
+
+class _ResidenceComplexListState extends State<ResidenceComplexList> {
+  @override
+  Widget build(BuildContext context) {
+    return 
+      Column(
+        children: [  
+            Row(
+              children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(239, 255, 218, 1),
+                      borderRadius: BorderRadius.circular(10),
+                      border: const BorderDirectional(
+                        bottom: BorderSide(color: Color.fromRGBO(118, 197, 19, 1)),
+                        top: BorderSide(color: Color.fromRGBO(118, 197, 19, 1)),
+                        start: BorderSide(color: Color.fromRGBO(118, 197, 19, 1)),
+                        end: BorderSide(color: Color.fromRGBO(118, 197, 19, 1)),
+                      ),
+                    ),
+                    margin: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.8, // 10% ширины экрана слева и справа
+                      top: MediaQuery.of(context).size.height * 0.05, // 5% высоты экрана сверху и снизу
+                    ),
+                    child: Expanded( child: IconButton(
+                      icon:  
+                        SvgPicture.asset(
+                           'assets/svg/build-filter.svg',
+                            width: 20,
+                            height: 20,
+                        ),
+                      onPressed: () {
+                         print("pushed filter buttom");
+                         Navigator.of(context).pushNamed('/rest-commun-filter', arguments: {});
+                      },
+                    )
+                  ))
+              ],
+            ),
+            Expanded(
+                  child: ListView.builder(
+                    itemCount: 8,
+                    // этого элемент листа
+                    itemBuilder: (context, i) {
+                      return const Build();
+                    })
+            ),
+        ],
+      );
+  }
+}
+
